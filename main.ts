e9e03c0e4fc8b6105f195b963ad8c54fd0311674
abder45400 <97219626+abder45400@users.noreply.github.com>
2022-01-28 15:46:31 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,27 @@
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (lastValue != control.eventValue()) {
         lastValue = control.eventValue()
+        vitesseM2 = 255
+        vitesseM1 = 255
         led.stopAnimation()
         if (control.eventValue() == 1) {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, vitesseM1)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, vitesseM2)
             basic.showString("A")
         } else if (control.eventValue() == 3) {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, vitesseM1)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, vitesseM2)
             basic.showString("B")
         } else if (control.eventValue() == 5) {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, vitesseM1)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, vitesseM2)
             basic.showString("C")
         } else if (control.eventValue() == 7) {
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, vitesseM1)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, vitesseM2)
             basic.showString("D")
         } else if (control.eventValue() == 9) {
+            motor.motorStopAll()
             basic.showString("1")
         } else if (control.eventValue() == 11) {
             basic.showString("2")
@@ -23,6 +34,8 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
         }
     }
 })
+let vitesseM1 = 0
+let vitesseM2 = 0
 let lastValue = 0
 lastValue = 0
 bluetooth.startLEDService()
